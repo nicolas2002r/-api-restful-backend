7f57de5918d8066cdb4ba23a30b36d4cb933a073
Nicolas Rivas <nrivas20201@corhuila.edu.co>
2024-09-05 10:33:20 -0500

    init project

---
+++ src/main/java/com/corhuila/scotiabank/api_restful_backend/ApiRestfulBackendApplication.java
@@ -0,0 +1,13 @@
+package com.corhuila.scotiabank.api_restful_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corhuila/scotiabank/api_restful_backend/ApiRestfulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corhuila.scotiabank.api_restful_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
