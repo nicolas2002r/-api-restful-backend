c83218f32d2ec4138a335b6f337edd03721fa500
Nicolas Rivas Meneses <nicolasrivasmeneses23@gmail.com>
2024-09-29 18:58:11 -0500

    CrosOrigin

--- src/main/java/com/corhuila/scotiabank/api_restful_backend/controller/UsuarioController.java
+++ src/main/java/com/corhuila/scotiabank/api_restful_backend/controller/UsuarioController.java
@@ -6,13 +6,11 @@ import com.corhuila.scotiabank.api_restful_backend.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
 
